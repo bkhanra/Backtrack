a027d5a99fdac618898db504da167e21fc46bd37
Bishwajit Khanra <42738731+bkhanra@users.noreply.github.com>
2024-08-20 19:49:05 +0530

    Add files via upload

---
+++ Maze.java
@@ -0,0 +1,16 @@
+package backtrack;
+
+public class Maze {
+	
+	public static boolean ratIntMaze(int maze[][]) {
+		
+	}
+
+	public static void main(String[] args) {
+		int[][] maze = {{1,1,0},{1,1,0},{1,1,1}};
+		boolean hasPath = ratIntMaze(maze);
+		System.out.println(hasPath);
+
+	}
+
+}
