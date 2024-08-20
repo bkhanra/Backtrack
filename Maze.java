518718121f039a9959a8a7b58b051e67697c1e68
Bishwajit Khanra <42738731+bkhanra@users.noreply.github.com>
2024-08-20 19:51:40 +0530

    Add files via upload

--- Maze.java
+++ Maze.java
@@ -8,8 +8,8 @@ public class Maze {
 
 	public static void main(String[] args) {
 		int[][] maze = {{1,1,0},{1,1,0},{1,1,1}};
-		boolean hasPath = ratIntMaze(maze);
-		System.out.println(hasPath);
+		boolean pathPossible = ratIntMaze(maze);
+		System.out.println(pathPossible);
 
 	}
 
