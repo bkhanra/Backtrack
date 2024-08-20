e6207237b2994d073076ec3115db79f7333c7fcd
Bishwajit Khanra <42738731+bkhanra@users.noreply.github.com>
2024-08-20 20:15:15 +0530

    Add files via upload

--- Maze.java
+++ Maze.java
@@ -3,12 +3,26 @@ package backtrack;
 public class Maze {
 	
 	public static boolean ratIntMaze(int maze[][]) {
+		int n = maze.length;
+		int[][] path = new int[n][n];
+		return solveMaze(maze,0,0,path);
 		
 		
 		
 	}
 	
 	public static boolean solveMaze(int[][] maze, int i, int j, int[][] path) {
+		//check if i, j cell is valid
+		int n = maze.length;
+		if(i < 0 || i >= n || j < 0 || j >= n || maze[i][j]==0 || path[i][j]==1) {
+			return false;
+		}
+		
+		if(i==n-1 && j==n-1){
+			
+			return true;
+		}
+		
 		
 	}
 
